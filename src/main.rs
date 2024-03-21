b0de0ef43c592016402ce6289a7d4f067f4c4e8f
Fernando Fernandez Mancera <ffmancera@riseup.net>
2024-03-21 22:07:24 +0100

    clap: add initial clap structure for netvisor binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let app = clap::Command::new("netvisor")
+        .version(clap::crate_version!())
+        .author("Fernando F. Mancera <ffmancera@riseup.net>")
+        .about("NetVisor command line tool")
+        .subcommand_required(true)
+        .subcommand(
+            clap::Command::new("show")
+            .about("Show a picture of the current network topology")
+        );
 }
