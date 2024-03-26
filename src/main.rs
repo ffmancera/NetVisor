324a1e798a4e6ed47dec3f4afbe7f4551e610d44
Fernando Fernandez Mancera <ffmancera@riseup.net>
2024-03-26 17:52:46 +0100

    clap: use clap derive workflow
    Creating the application declaratively with a struct and attributes
    seems a better idea.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
 mod network;
 
+use clap::{Parser, Subcommand};
+
 use crate::network::query_network;
 
-fn main() {
-    let app = clap::Command::new("netvisor")
-        .version(clap::crate_version!())
-        .author("Fernando F. Mancera <ffmancera@riseup.net>")
-        .about("NetVisor command line tool")
-        .subcommand_required(true)
-        .subcommand(
-            clap::Command::new("show")
-            .about("Show a picture of the current network topology")
-        );
+/// Host network topology visualization tool.
+#[derive(Parser)]
+#[command(version, about)]
+struct Cli {
+    /// Turn on debug logs
+    #[arg(short, long)]
+    debug: bool,
 
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Shows the diagram of the network topology
+    Show,
+}
+
+fn main() {
+    let cli = Cli::parse();
     println!("{:?}", query_network().unwrap());
 }
