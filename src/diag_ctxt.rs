881ca92ed56dbde37cfe10bd45189ead3caf2b58
Fernando Fernandez Mancera <ffmancera@riseup.net>
2024-03-30 23:05:05 +0100

    diag_ctxt: introduce IfaceCtxt struct
    Each interface will have each own context for representation.

--- src/diag_ctxt.rs
+++ src/diag_ctxt.rs
@@ -1,7 +1,14 @@
 use gethostname::gethostname;
+use nispor::Iface;
+
+struct IfaceCtxt {
+    iface: Iface,
+    depth: u32,
+}
 
 pub struct DiagramCtxt {
     pub hostname: String,
+    ifaces: Vec<IfaceCtxt>,
 }
 
 impl DiagramCtxt {
@@ -10,6 +17,6 @@ impl DiagramCtxt {
             Some(v) => v.to_string(),
             None => "host".to_string(),
         };
-        DiagramCtxt { hostname: host }
+        DiagramCtxt { hostname: host, ifaces: Vec::new() }
     }
 }
