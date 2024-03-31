0099e07cb78a6306596437899dc578426ceb19ca
Fernando Fernandez Mancera <ffmancera@riseup.net>
2024-03-31 11:40:50 +0200

    graphics: represent interfaces in the same level
    Now, interfaces are represented dynamically. Next step is to represent
    them considering the depth.

--- src/diag_ctxt.rs
+++ src/diag_ctxt.rs
@@ -3,11 +3,13 @@ use nispor::{Iface, NisporError};
 
 use crate::network::query_network;
 
+#[derive(Clone)]
 pub struct IfaceCtxt {
     pub iface: Iface,
-    depth: u32,
+    pub depth: u32,
 }
 
+#[derive(Clone)]
 pub struct DiagramCtxt {
     pub hostname: String,
     pub ifaces: Vec<IfaceCtxt>,
@@ -26,12 +28,19 @@ impl DiagramCtxt {
             .cloned()
             .map(|np_iface| IfaceCtxt {
                 iface: np_iface,
-                depth: 0,
+                depth: 1,
             })
             .collect();
         Ok(DiagramCtxt {
             hostname: host,
             ifaces: iface_ctxts,
         })
+    }
+
+    pub fn count_ifaces_for_depth(self, depth: u32) -> usize {
+        self.ifaces
+            .iter()
+            .filter(|iface| iface.depth == depth)
+            .count()
     }
 }
--- src/graphics.rs
+++ src/graphics.rs
@@ -5,7 +5,13 @@ use crate::diag_ctxt::{DiagramCtxt, IfaceCtxt};
 
 pub fn draw_picture(diag_ctx: DiagramCtxt, file: &String) {
     // TODO: the right size is hard to guess, make this dynamic somehow
-    let surface = ImageSurface::create(Format::ARgb32, 1280, 720).expect("Couldn’t create surface");
+    let initial_size = calculate_size(&diag_ctx);
+    let surface = ImageSurface::create(
+        Format::ARgb32,
+        (initial_size.0 as i32) + 400,
+        (initial_size.1 as i32) + 200,
+    )
+    .expect("Couldn’t create surface");
     let context = Context::new(&surface).unwrap();
 
     // Set a white background
@@ -13,42 +19,72 @@ pub fn draw_picture(diag_ctx: DiagramCtxt, file: &String) {
     context.paint().unwrap();
 
     // Draw initial box representing the host
+    let initial_pos = (200., 100.);
     rectangle_with_border(
         &context,
-        (190., 110.),
-        (900., 500.),
+        initial_pos,
+        initial_size,
         (0.9765625, 0.98046875, 0.58203125),
     );
-    write_text_top_right_corner_rect(
-        &context,
-        (190., 110.),
-        (900., 500.),
-        25.,
-        &diag_ctx.hostname,
-    );
+    write_text_top_right_corner_rect(&context, initial_pos, initial_size, 25., &diag_ctx.hostname);
 
     // TODO: draw all the interfaces, this is just some initial work
-    draw_iface_rect(&context, diag_ctx.ifaces.get(0).unwrap(), (190., 110.));
+    let mut count: u32 = 0;
+    for iface in diag_ctx.clone().ifaces {
+        draw_iface_rect(
+            &context,
+            &iface,
+            initial_pos,
+            initial_size,
+            diag_ctx.clone().count_ifaces_for_depth(1) as u32,
+            count,
+        );
+        count = count + 1;
+    }
+
     let mut file = File::create(file).expect("Couldn’t create file");
     surface
         .write_to_png(&mut file)
         .expect("Couldn’t write to png");
 }
 
-fn draw_iface_rect(context: &Context, iface_ctx: &IfaceCtxt, rect_pos: (f64, f64)) {
+fn draw_iface_rect(
+    context: &Context,
+    iface_ctx: &IfaceCtxt,
+    rect_pos: (f64, f64),
+    rect_size: (f64, f64),
+    total: u32,
+    count: u32,
+) {
+    let iface_width = 300.;
+    let iface_height = 100.;
+    let pos = (
+        rect_pos.0
+            + ((rect_size.0 / total as f64) / 2. - iface_width / 2.)
+            + ((count as f64) * rect_size.0 / (total as f64)),
+        rect_pos.1 + (rect_size.1 / ((iface_ctx.depth as f64) + 1.) - (iface_height / 2.)),
+    );
     rectangle_with_border(
         &context,
-        (rect_pos.0 + 25., rect_pos.1 + 370.),
-        (300., 100.),
+        pos,
+        (iface_width, iface_height),
         (0.5703125, 0.7734375, 1.),
     );
     write_text_center_rect(
         &context,
-        (rect_pos.0 + 25., rect_pos.1 + 370.),
-        (300., 100.),
+        pos,
+        (iface_width, iface_height),
         25.,
         &iface_ctx.iface.name,
     );
+}
+
+fn calculate_size(diag_ctx: &DiagramCtxt) -> (f64, f64) {
+    let mut size = (300., 300.);
+    for _ in diag_ctx.ifaces.clone() {
+        size.0 = size.0 + 300.;
+    }
+    size
 }
 
 fn rectangle_with_border(
